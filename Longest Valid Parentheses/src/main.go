97feb2d5b258658a4806ba71eabe6b0069b994a8
trustyred <244073249@qq.com>
2019-02-26 22:20:05 +0800

    add Longest Valid Parentheses golang Stack solution

--- Longest Valid Parentheses/src/main.go
+++ Longest Valid Parentheses/src/main.go
@@ -1,14 +1,66 @@
 package main
 
-import "fmt"
-
+import (
+	"container/list"
+	"fmt"
+)
+type Stack struct {
+	list *list.List
+}
 func main() {
 	//fmt.Println(longestValidParentheses(")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"))
+	//fmt.Println(longestValidParentheses(")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"))
 	fmt.Println(longestValidParentheses("((()))"))
-
 }
+func NewStack() *Stack{
+	l := list.New()
+	stack := Stack{l}
+	return &stack
+}
+func (stack *Stack)Push(value interface{}){
+	stack.list.PushBack(value)
+}
+func (stack *Stack)Pop() interface{}{
+	e := stack.list.Back()
+	if e!= nil{
+		stack.list.Remove(e)
+		return e.Value
+	}
+	return nil
+}
+func (stack *Stack)Empty() bool{
+	return stack.list.Len() == 0
+}
+func (stack *Stack)Peek() interface{}{
+	e := stack.list.Back()
+	if e != nil{
+		return e.Value
+	}
+	return nil
+}
+func longestValidParentheses(s string) int{
+	stack := NewStack()
+	max := 0
+	stack.Push(-1)
+	for i:=0;i<len(s);i++{
+		if s[i] == '('{
+			stack.Push(i)
+		}else if s[i] == ')'{
+			stack.Pop()
+			if stack.Empty(){
+				stack.Push(i)
+			}else{
+				if i-stack.Peek().(int) > max{
+					max = i - stack.Peek().(int)
+				}
+			}
+		}
 
-func longestValidParentheses(s string) int {
+	}
+
+	return max
+}
+func longestValidParentheses_dp(s string) int {
 	var dp [99999999]int
 	if len(s) == 0 {
 		return 0
