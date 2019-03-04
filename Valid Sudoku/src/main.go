32f76dee0c03d51d1da9187b49a3e2d9de14d93a
trustyred <244073249@qq.com>
2019-03-04 16:36:15 +0800

    add Valid Sudoku golang solution

--- Valid Sudoku/src/main.go
+++ Valid Sudoku/src/main.go
@@ -8,14 +8,31 @@ type Set struct {
 }
 
 func main() {
-	m := map[int]int{}
-	m[1] = 2
-	_,ok := m[1]
-	if ok{
-		fmt.Println("have")
-	}else{
-		fmt.Println("no")
+	fmt.Printf("%d\n",'.')
+	sudo := [][]byte{
+		{'.','8','7','6','5','4','3','2','1'},
+		{'2','.','.','.','.','.','.','.','.'},
+		{'3','.','.','.','.','.','.','.','.'},
+		{'4','.','.','.','.','.','.','.','.'},
+		{'5','.','.','.','.','.','.','.','.'},
+		{'6','.','.','.','.','.','.','.','.'},
+		{'7','.','.','.','.','.','.','.','.'},
+		{'8','.','.','.','.','.','.','.','.'},
+		{'9','.','.','.','.','.','.','.','.'},
 	}
+	//sudo := [][]byte{
+	//	{'8','3','.','.','7','.','.','.','.'},
+	//	{'6','.','.','1','9','5','.','.','.'},
+	//	{'.','9','8','.','.','.','.','6','.'},
+	//	{'8','.','.','.','6','.','.','.','3'},
+	//	{'4','.','.','8','.','3','.','.','1'},
+	//	{'7','.','.','.','2','.','.','.','6'},
+	//	{'.','6','.','.','.','.','2','8','.'},
+	//	{'.','.','.','4','1','9','.','.','5'},
+	//	{'.','.','.','.','8','.','.','7','9'},
+	//}
+	result := isValidSudoku(sudo)
+	fmt.Println(result)
 }
 //进行编码操作
 //行数据编码成()line_number
@@ -23,7 +40,7 @@ func main() {
 //3*3方块内数据编码成(x)(y)
 func isValidSudoku(board [][]byte) bool {
 
-	var sudokuMap map[string]Set
+	sudokuMap := make(map[string]*Set)
 
 	for i:= 0;i<9;i++{
 		for j:=0 ;j<9;j++{
@@ -35,10 +52,57 @@ func isValidSudoku(board [][]byte) bool {
 			set2,ok2 := sudokuMap[colIndex]
 			set3,ok3 := sudokuMap[localIndex]
 			if ok1{
-				set1.Add()
+
+				ret := set1.Add(board[i][j])
+				if !ret && board[i][j]!='.'{
+					return false
+				}
+			}else{
+				var s *Set
+				s = New()
+				sudokuMap[lineIndex] = s
+				s.Add(board[i][j])
+
 			}
+			if ok2 {
+
+				ret := set2.Add(board[i][j])
+				if !ret && board[i][j]!='.'{
+					return false
+				}
+			}else{
+				var s *Set
+				s = New()
+				sudokuMap[colIndex] = s
+				s.Add(board[i][j])
+			}
+			if ok3 {
+				ret := set3.Add(board[i][j])
+				if !ret && board[i][j]!='.'{
+					return false
+				}
+			}else{
+				var s *Set
+				s = New()
+				sudokuMap[localIndex] = s
+				s.Add(board[i][j])
+
+			}
+
 		}
 	}
+	//fmt.Println(sudokuMap)
+	//flagCount := 0
+	//for _,v:= range sudokuMap{
+	//	if len(v.Map) == 1{
+	//		flagCount ++
+	//	}
+	//}
+	//if flagCount == 27||flagCount==0{
+	//	return true
+	//}else{
+	//	return false
+	//}
 	return true
 }
 
@@ -49,5 +113,11 @@ func (set *Set) Add(elem interface{}) bool{
 	}else{
 		set.Map[elem] = 1
 		return true
+	}
+}
+
+func New() *Set{
+	return &Set{
+		Map: map[interface{}]int{},
 	}
 }
