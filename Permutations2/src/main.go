449414dd67d1f055fa0053111652587eccc0b001
trustyred <244073249@qq.com>
2019-03-12 19:46:02 +0800

    add Permutations golang solution and debug very long times

--- Permutations2/src/main.go
+++ Permutations2/src/main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	result := permute([]int{1,1,2})
+	result := permuteUnique([]int{-1,2,-1,2,1,-1,2,1})
 	fmt.Println(result)
 }
 
-func permute(nums []int) [][]int {
+func permuteUnique(nums []int) [][]int {
+	sort.Ints(nums)
 	result := backtrack([]int{},nums,[][]int{},len(nums))
 	return result
 }
 
 func backtrack(current []int,remain []int, result [][]int, n int)[][]int{
 	if len(current) == n{
-
-		result = append(result, current)
+		//这里真的是golang里面的坑，真是不长记性，按照这种添加方法，我的result是会变化的，因为result与current共享了
+		//底层数组，这种问题还终极不好排查
+		tmp := make([]int,len(current))
+		copy(tmp,current)
+		result = append(result, tmp)
 		return result
 	}
 	for i:=0;i<len(remain);i++{
+		if i<len(remain)-1 && remain[i+1] == remain[i]{
+			continue
+		}
 		c := append(current,remain[i])
 		r := sliceDel(remain,i)
 		result = backtrack(c,r,result,n)
