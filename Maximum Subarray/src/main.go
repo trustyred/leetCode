793fa9c289a7c8b0a6d6f8bf91edaffc7781304e
trustyred <244073249@qq.com>
2019-03-17 11:31:33 +0800

    add Maximum Subarray golang solution

--- Maximum Subarray/src/main.go
+++ Maximum Subarray/src/main.go
@@ -1,20 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	result := maxSubArray([]int{1,2})
+	fmt.Println(result)
 }
 
 func maxSubArray(nums []int) int {
-	if len(nums) == 0{
-		return 0
-	}
+	dp := make([]int,len(nums))
 	max := nums[0]
-	maxIndex := 0
+	dp[0] = nums[0]
 	for i:=1;i<len(nums);i++{
-		if nums[i] > max{
-			maxIndex = i
-			max = nums[i]
+		addNum := 0
+		if dp[i-1] >0{
+			addNum = dp[i-1]
+		}else{
+			addNum = 0
+		}
+		dp[i] = nums[i] + addNum
+		if dp[i] > max{
+			max = dp[i]
 		}
 	}
-
+	return max
 }
+
+
