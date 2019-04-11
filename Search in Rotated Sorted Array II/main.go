fde1dd687a73b47451d76e3b9de4fef90186b76f
trustyred <244073249@qq.com>
2019-04-11 23:03:19 +0800

    add Search in Rotated Sorted Array II golang solution

--- Search in Rotated Sorted Array II/main.go
+++ Search in Rotated Sorted Array II/main.go
@@ -5,36 +5,35 @@ import (
 )
 
 func main() {
-	result := search([]int{1,3},3)
+	result := search([]int{1,1,3},1)
 	fmt.Println(result)
 }
 func search(nums[]int, target int)bool{
-	var realMid int
-	if len(nums) ==0{
-		return false
-	}
 	lo := 0
-	hi := len(nums) - 1
-	for lo < hi {
-		mid := (lo+hi)/2
-		if (nums[mid] < nums[0]) && (target < nums[0])||((nums[mid] > nums[0]) && (target >nums[0])){
-			realMid = nums[mid]
-		}else if target < nums[0]{
-			realMid = -653524738
-		}else {
-			realMid = 653524738
+	hi := len(nums)-1
+	for lo<=hi{
+		for lo<hi && nums[lo] == nums[hi]{
+			lo++
 		}
-
-		if target > realMid {
-			lo ++
-		}else if target < realMid {
-			hi = mid
-		}else{
+		mid := (lo+hi)/2
+		if target == nums[mid]{
 			return true
 		}
-	}
-	if nums[(lo+hi)/2] == target{
-		return true
+		if nums[mid] > nums[lo]{
+			if target < nums[mid] && nums[lo] <= target{
+				hi = mid
+			}else{
+				lo++
+			}
+		}else if nums[mid] < nums[lo]{
+			if target > nums[mid]  && nums[hi] >= target{
+				lo++
+			}else{
+				hi = mid
+			}
+		}else{
+			lo++
+		}
 	}
 	return false
 }
