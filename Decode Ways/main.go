c4712deb6e3f8cadecc00a4e5eaf83fb85dd39c6
trustyred <244073249@qq.com>
2019-04-19 13:29:11 +0800

    add Decode Ways golang dp solution

--- Decode Ways/main.go
+++ Decode Ways/main.go
@@ -2,43 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	result := numDecodings("0")
+	result := numDecodings("226")
 
 	fmt.Println(result)
 }
 func numDecodings(s string) int {
-	if len(s) == 0{
-		return 0
+	dp := make([]int,len(s)+1)
+
+	dp[len(s)] = 1
+	if s[len(s)-1] =='0'{
+		dp[len(s)-1] = 0
 	}else{
-		if s[0] == '0'{
-			return 0
-		}
+		dp[len(s)-1] = 1
 	}
-
-	dp := make([]int,len(s))
-	for i:=0;i<len(s);i++{
-		if i==0{
-			if s[i] == '0'{
-				dp[i] = 0
-			}else{
-				dp[i] = 1
+	for i:=len(s)-2;i>=0;i--{
+		if s[i] == '0'{
+			continue
+		}else{
+			num,err := strconv.Atoi(s[i:i+2])
+			if err==nil && num<=26{
+				dp[i] = dp[i+1]+dp[i+2]
+			}else if err==nil && num>26{
+				dp[i] = dp[i+1]
 			}
-		}else if i>0 && s[i] != '0' && (s[i-1]>='0'&&s[i-1]<'3') && (s[i]>'0'&&s[i]>'7'){
-			/*
-			1 : 1
-			1,2: 1,2 12
-			1,2,2: 1,2,2 12,2 1,22
-			1,2,2,2: 1,2,2,2 12,2,2 1,22,2 1,2,22 12,22
-			1,2,2,2,2: 1,2,2,2,2 12,2,2,2 1,22,2,2 1,2,22,2 12,22,2 1,2,2,22 12,2,22 1,22,22
-			 */
-			dp[i] =
-
 		}
 	}
-
-	fmt.Println(dp)
-	return dp[len(s)]
+	return dp[0]
 }
